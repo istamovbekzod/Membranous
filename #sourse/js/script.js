328624a3da1da63bc158e8920766ac92380caad9
Истамов Бекзод <94726391+istamovbekzod@users.noreply.github.com>
2023-03-28 19:26:50 +0300

    Add files via upload

---
+++ #sourse/js/script.js
@@ -0,0 +1,16 @@
+//SLIDER==================================================================================================
+$(document).ready(function () {
+	$('#pagepiling').pagepiling({
+		scrollingSpeed: 100,
+	});
+});
+//LOADER==================================================================================================
+document.body.onload = function () {
+	setTimeout(function () {
+		var preloader = document.getElementById('page-preloader');
+		if (!preloader.classList.contains('done')) {
+			preloader.classList.add('done');
+		}
+	}, 2000)
+}
+//========================================================================================================
---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+//SLIDER==================================================================================================
+$(document).ready(function () {
+	$('#pagepiling').pagepiling({
+		scrollingSpeed: 100,
+	});
+});
+//LOADER==================================================================================================
+document.body.onload = function () {
+	setTimeout(function () {
+		var preloader = document.getElementById('page-preloader');
+		if (!preloader.classList.contains('done')) {
+			preloader.classList.add('done');
+		}
+	}, 2000)
+}
+//========================================================================================================
